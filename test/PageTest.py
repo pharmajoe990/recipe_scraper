eccdaf9e577c00eb65e40db392ac758f94a0a33c
pharmajoe990 <tim.r.roper@gmail.com>
2014-05-02 08:03:29 +1000

    Check for each parsed URL now working.

--- test/PageTest.py
+++ test/PageTest.py
@@ -12,7 +12,7 @@ class PageTest(unittest.TestCase):
         html = file("../miscellany/sausage_and_punpkin_curry.html").read()
         urls = Page.get_urls(html)
         self.assertEquals(len(urls), 387)
-        a = re.compile("'http://www\.[//\a\w\.\+]+'")
-        # for url in urls:
-        #     # Check each URL matches a hyperlink pattern
-        #     self.assertTrue(a.match(url))
+        a = re.compile(r'http://www\.[//\a\w\.\+]+')
+        for url in urls:
+            # Check each URL matches a hyperlink pattern
+            self.assertTrue(a.match(url))
