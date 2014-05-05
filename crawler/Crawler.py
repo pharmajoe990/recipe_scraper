02c8565476f2983dcc7d3eca539f6aa85bb0ceb6
pharmajoe990 <tim.r.roper@gmail.com>
2014-05-05 21:42:44 +1000

    Only add the correct URLs to the list

--- crawler/Crawler.py
+++ crawler/Crawler.py
@@ -13,6 +13,9 @@ The Crawler operates thus
 2.  Parse the document at the base URL for any child pages in the base URL.
 3.  Parse the child pages, one at a time, for any further child pages up to the
     specified depth.
+
+Notes
+    -If children is set to 0, Crawler will scan ALL pages.
 """
 import re
 
@@ -28,35 +31,52 @@ class Crawler(object):
         self.depth = crawl_depth
         self.children = children
 
-    def check_and_add(self, url):
-        # Check if the URL matches a pattern for a recipe.
-        if re.match(r'http://www.taste.com.au/recipes/\d+/[\w+]+', url):
-            if url not in self.urls:
-                self.urls.append(url)
-
     def build_crawl_list(self):
         """
         Build a list of all of the URLs based on the depth specified.
         """
         current_depth = 1
         page = requests.get(self.base_url).text
-        self.urls = Page.get_urls(page)[:self.children]
+        if self.children > 0:
+            self.urls = Page.get_urls(page)[:self.children]
+        else:
+            self.urls = Page.get_urls(page)
+        # Below stops URLs being added twice
         scanned_urls = []
         while current_depth <= self.depth:
             # Append the links for each page then search it for more
             print 'Starting crawl depth', current_depth, 'with', len(self.urls), 'URLs to scan'
-            new_pages = []
+            new_urls = []
             for url in self.urls:
+                #todo Don't bother scanning images, css etc.
                 if url not in scanned_urls:
                     print 'Looking for child URLs in ', url
                     markup = requests.get(url).text
                     scanned_urls.append(url)
-                    new_pages = Page.get_urls(markup)[:self.children]
-            print 'Found', len(new_pages), 'new pages'
-            # Below is adding the whole list. Not appending
-            self.urls += new_pages
+                    if self.children > 0:
+                        new_urls = Page.get_urls(markup)[:self.children]
+                    else:
+                        new_urls = Page.get_urls(markup)
+            print 'Found', len(new_urls), 'new pages'
+            for url in new_urls:
+                self.check_and_add(url)
+            # self.urls += new_urls
             current_depth += 1
         print 'Finished crawling', self.base_url, 'found', len(self.urls), 'total URLs'
+
+    def get_urls(html):
+        """
+        Parse the html passed to this method and find all the URLs or
+        Hyperlinks within the markup. This includes all links, such
+        as images and other URLs (not restricted).
+        """
+        return re.findall(r'http://www\.[//\a\w\.\+]+', html)
+
+    def check_and_add(self, url):
+        # Check if the URL matches a pattern for a recipe.
+        if re.match(r'http://www.taste.com.au/recipes/\d+/[\w+]+', url):
+            if url not in self.urls:
+                self.urls.append(url)
 
     def run(self):
         """
--- run.py
+++ run.py
@@ -4,14 +4,19 @@ Run the program.
 import pprint
 
 from crawler.Crawler import Crawler
+from scraper.TasteDotCom import TasteDotCom
 
 
 page = "http://www.taste.com.au/"
 #todo below is redundant as depth not used
-depth = 5
-children = 30
+depth = 3
+children = 0
 
 crawler = Crawler(page, depth, children)
 crawler.build_crawl_list()
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(crawler.urls)
+recipes = []
+for url in crawler.urls:
+    t = TasteDotCom(url)
+    recipes.append()
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(crawler.urls)
